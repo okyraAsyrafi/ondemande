4dd0398ad7239568f7e53fd2ae672e00176d089d
okyra <okyra19chan@gmail.com>
2020-02-14 08:12:41 +0700

    lanjutan dari mengirim notifikasi

--- cobasatu/FCM.php
+++ cobasatu/FCM.php
@@ -24,16 +24,25 @@ class FCM
 
 //        firebase api key
         $headers = array(
-            'Authorization:key=',
+            'Authorization:key=AAAAZR7ydec:APA91bHJJpmI15oA3NaytcjnBfne_j5P-wKzKV7Vln5snZEg3c50utyupMJFhr-khWkx1UCzsnbr10myzaWAXFPKIgKwy7KshvFPw4tCK1o4TpzCyoVSTjp9i-5lbbOybNbaUWka7J_i',
             'Content-Type:application/json'
         );
 
 //        open connection
         $curl = curl_init();
+//        set the url, number of POST vars, POST data
+        curl_setopt($curl,CURLOPT_URL,$url);
+        curl_setopt($curl,CURLOPT_POST,true);
+        curl_setopt($curl,CURLOPT_HTTPHEADER,$headers);
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+//        disable sertificate support temporary SSL
+        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($isifields));
 
-
-
-
-
+        $result = curl_exec($curl);
+        if ($result == FALSE){
+            die('Curl Failed : '.curl_error($curl));
+        }
+        curl_close($curl);
     }
 }
--- cobasatu/pushFCM.php
+++ cobasatu/pushFCM.php
@@ -5,8 +5,22 @@
  * Date: 12/02/2020
  * Time: 14:07
  */
+$tokenHp = "dQikCFdwODY:APA91bHTAb2o_xUqC8n79bxuoWRXDoANS8sgyLxabuMB1w-790qXmOdwK5HBRQC6MwTdy7U6r-GGgNbYgtY96a2-avYswP-U3SE4AHpPQa1YhTzItrq0gWFrZgCgtc_crw4vbaX1Ljc9";
+include_once 'FCM.php';
 
 //ambil data inputan
 $titNotif = $_POST['titleNotifikasi'];
-$subNotif = $_POST['subNotifikasi'];
+//$subNotif = $_POST['subNotifikasi'];
 $isiNotif = $_POST['isiNotifikasi'];
+
+$notification = array();
+$arrData = array();
+$arrNotification =array(
+    'body' => $isiNotif,
+    'title' => $titNotif,
+    'sound' => 'default',
+    'type' => '1'
+);
+
+$fcm = new FCM();
+$result = $fcm->send_notification($tokenHp,$arrNotification);
